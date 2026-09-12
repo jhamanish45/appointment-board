def83ba507ff680130907039ccd22e4ac4f97371
Manish Jha <mjha6740@gmail.com>
2026-09-12 13:42:14 +0530

    Allow Vercel frontend CORS

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
        " https://appointment-boards.vercel.app"
     ],
     allow_credentials=True,
